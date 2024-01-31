3c2eb4402d1be084459d8c88a93ecc67df736271
satan-pro <sayantancreator@gmail.com>
2024-01-31 22:44:56 +0530

    Note taking feature added

--- src/components/Body/Notes/Notes.jsx
+++ src/components/Body/Notes/Notes.jsx
@@ -1,48 +1,68 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import ContentCards from "./ContentCards";
 import { weekday, day, monthShort } from "../../../data/date";
-import Icon from "../Icon";
-import icons from "../../../data/icons";
 import Fab from '@mui/material/Fab';
 import AddIcon from '@mui/icons-material/Add';
+import Zoom from '@mui/material/Zoom';
 import notes from "../../../data/notes";
 
 function Notes() {
   let date = `${weekday}, ${monthShort} ${day}`;
 
   const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
   const [notesList, setNotesList] = useState([]);
+  const [clicked, checkClicked] = useState(false);
 
-  function titleChange(e)
-  {
-    let titleValue = e.target.value;
-    setTitle(titleValue);
+  function handleChange(e) {
+    let inputName = e.target.name;
+    let inputValue = e.target.value;
+    if (inputName === "titleInput") {
+      setTitle(inputValue);
+    }
+    else if (inputName === "descInput") {
+      setDesc(inputValue);
+    }
   }
 
-  function handleClick()
-  {
-    let newNote={
-      title:title,
-      content:"This is me"
+  function handleClick() {
+    let newNote = {
+      title: title,
+      content: desc
     };
-    setNotesList((prev)=>{
+    setNotesList((prev) => {
       return [...prev, newNote]
     });
     notes.push(newNote);
     setTitle("");
+    setDesc("");
+  }
+
+  function titleClick(e) {
+    checkClicked(() => {
+      return !clicked;
+    })
+  } 
+
+  function titleUnclick()
+  {
+    if(clicked)
+    {
+      checkClicked(false);
+    }
   }
 
   return (
-    <div class="notes">
+    <div class="notes" onClick={titleUnclick}>
       <div className="input-box">
-        <input type="text" name="notesInput" placeholder="Title" value={title} onChange={titleChange}/>
-        <textarea placeholder="Type your note" />
+        <input type="text" name="titleInput" placeholder="Title" value={title} onChange={handleChange} onClick={titleClick} />
+        <textarea placeholder="Type your note" name="descInput" onChange={handleChange} value={desc} style={clicked ? { display: "block" } : { display: "none" }} />
         <div className="input-button-area">
-          
+          <Zoom in={clicked ? true : false} >
             <Fab color="success" aria-label="add" size="small" onClick={handleClick}>
-                <AddIcon />
-              </Fab>
-         
+              <AddIcon />
+            </Fab>
+          </Zoom>
         </div>
       </div>
       <div class="headline">
